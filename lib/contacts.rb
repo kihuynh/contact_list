43e8168cfc0fe1750a6ceb6296d0f3c96be29fe6
Alex Ficklin and Kimberly Huynh <alex@>
2018-02-15 09:10:57 -0800

    write second spec to save

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -3,15 +3,19 @@
 class Contact
   @@list = []
 
-  def initialize(name1, name2, address, job)
+  def initialize(name1)
     @name1 = name1
-    @name2 = name2
-    @address = address
-    @job = job
+    # @name2 = name2
+    # @address = address
+    # @job = job
   end
 
 
   def self.all
     @@list
   end
+
+  # def save
+  #   @@list.push(self)
+  # end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -6,4 +6,10 @@ describe('contacts') do
   it("An empty list") do
   expect(Contact.all()).to(eq([]))
    end
+
+   it("Save an item to the list") do
+    name =  Contact.new("joe")
+    name.save()
+   expect(Contact.all()).to(eq([name1]))
+    end
 end
