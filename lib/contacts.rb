c11dd087dae0a31727855b608357658784e07e8b
Alex Ficklin and Kimberly Huynh <kimberly@>
2018-02-15 09:06:30 -0800

    pass first spec

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/ruby
 
 class Contact
+  @@list = []
 
+  def initialize(name1, name2, address, job)
+    @name1 = name1
+    @name2 = name2
+    @address = address
+    @job = job
+  end
+
+
+  def self.all
+    @@list
+  end
 end
-
